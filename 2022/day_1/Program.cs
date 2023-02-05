2ae3b9cfbf05049d172ed9e20444744675629311
Mike Nightengale <72324107+mikenightengale@users.noreply.github.com>
2023-02-05 16:31:38 -0500

    Added day one code

---
+++ 2022/day_1/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+List<int> currentElfCalories = new();
+List<int> elfCalories= new();
+
+foreach (string line in lines)
+{
+    if (!string.IsNullOrEmpty(line))
+    {
+        currentElfCalories.Add(int.Parse(line));
+        
+        if (line != lines.Last())
+        {
+            continue;
+        }
+    }
+
+    elfCalories.Add(currentElfCalories.Sum());
+    currentElfCalories.Clear();
+}
+
+var topElvesCalories = elfCalories.Order().OrderDescending().Take(3);
+Console.WriteLine($"PART ONE: Elf with most calories: {topElvesCalories.FirstOrDefault()}");
+Console.WriteLine($"PART TWO: Total calories of top three elves: {topElvesCalories.Sum()}");
